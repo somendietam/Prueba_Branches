f821b257e0be5f0c6eb8ccb2d6bf43d78868ec28
Bryan <zuluagabrayan099@gmail.com>
2024-09-23 18:39:08 -0500

    create view cart

--- prueba/prueba_app/views.py
+++ prueba/prueba_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from .models import *
 
-# Create your views here.
+def ver_cart(request):
+    """Muestra el carrito de compras del usuario."""
+    # Supongamos que el usuario está autenticado y lo obtenemos de la sesión
+    user_email = request.session.get('user_email')  # Aquí asumo que estás usando sesiones para almacenar el correo del usuario
+    cart = get_object_or_404(Cart, user=user_email)
+    
+    context = {
+        'cart': cart,
+    }
+    return render(request, 'cart/cart_detail.html', context)
